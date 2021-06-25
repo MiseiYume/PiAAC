9a40df9fe46639231e9ce4a6bc79a1b05c3e0865
Krzysztof Hodun <miseiyume@gmail.com>
2021-06-25 23:59:27 +0200

    Lab 4.00002137
    innit m8

--- run.py
+++ run.py
@@ -14,6 +14,9 @@ def gallery():
 @app.route("/contact.html")
 def contact():
     return render_template('contact.html')
+@app.route("/guestbook.html")
+def guestbook():
+    return render_template('guestbook.html')
 
 @app.route('/error_internal')
 def error_internal():
@@ -21,11 +24,6 @@ def error_internal():
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
-@app.route('/error_not_found')
-def error_not_found():
-    response = make_response(render_template('template.html', name='ERROR 404'), 404)
-    response.headers['X-Something'] = 'A value'
-    return response
 
 if __name__ == '__main__':
     app.run(debug = True)
